6dbfe2317a1ef558e9673843bc59c682c1c9441f
Stanislav <stasdamaratskiy@gmail.com>
2022-07-19 11:59:13 +0300

    add comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // Из-за проблем с java окружением (при запуске через терминал ничего не проиходит и ничего не выводит)
+        // Пришлось сделать выбор файла через консоль
+        // Как должна была выглядеть команда запуска программы:
+        // PS C:\krista-test> java .\Main.java C:\Code\krista-test\plants__001.xml
         // args[0]="plants__001.xml";
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите путь до файла:");
